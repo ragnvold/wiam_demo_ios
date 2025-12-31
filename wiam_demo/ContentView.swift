4940285ded82a4841191209cb3290e19117f43c4
Nikita Parmenov <parmenov.nik44@gmail.com>
2025-12-31 14:21:25 +0300

    Initial Commit

---
+++ wiam_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wiam_demo
+//
+//  Created by Nikita Parmenov on 31.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wiam_demo/wiam_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiam_demoApp.swift
+//  wiam_demo
+//
+//  Created by Nikita Parmenov on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct wiam_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
